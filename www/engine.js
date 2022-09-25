845ae5dd840e5f14b6f5d40c27f4dffe8fe56ad8
mapalomo <mangelespalomo@gmail.com>
2022-09-26 01:06:16 +0200

    stable version

--- www/engine.js
+++ www/engine.js
@@ -1,8 +1,35 @@
-document.addEventListener("contextmenu", function(event){
+document.addEventListener("contextmenu", function (event) {
     event.preventDefault();
 }, false);
 
 document.addEventListener("copy", (evt) => {
     evt.clipboardData.setData("text/plain", "");
-       evt.preventDefault();
-  }, false);
+    evt.preventDefault();
+}, false);
+
+document.addEventListener("keyup", function (e) {
+    var keyCode = e.keyCode ? e.keyCode : e.which;
+    if (keyCode == 44) {
+        stopPrntScr();
+    }
+});
+function stopPrntScr() {
+
+    var inpFld = document.createElement("input");
+    inpFld.setAttribute("value", ".");
+    inpFld.setAttribute("width", "0");
+    inpFld.style.height = "0px";
+    inpFld.style.width = "0px";
+    inpFld.style.border = "0px";
+    document.body.appendChild(inpFld);
+    inpFld.select();
+    document.execCommand("copy");
+    inpFld.remove(inpFld);
+}
+function AccessClipboardData() {
+    try {
+        window.clipboardData.setData('text', "Access   Restricted");
+    } catch (err) {
+    }
+}
+setInterval("AccessClipboardData()", 300);
